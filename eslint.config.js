f060ba32c4eb5c242998d114b0674fed4ef1f625
Ahmed Kemala <kemalaahmed@gmail.com>
2026-01-09 02:13:22 +0100

    init setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,35 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
-  expoConfig,
+  ...expoConfig,
+
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*", "node_modules/*", "ios/*", "android/*"],
+
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+        project: "./tsconfig.json",
+      },
+    },
+
+    plugins: {
+      prettier: require("eslint-plugin-prettier"),
+    },
+
+    rules: {
+      // Prettier
+      "prettier/prettier": "warn",
+
+      // React Native specific rules
+      "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "import/prefer-default-export": "off",
+    },
   },
 ]);
